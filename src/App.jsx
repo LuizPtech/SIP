c9f73bd0feda0b824487e35a379146ab84ba6b48
Luiz  Phelipe <luizphelipe123@hotmail.com>
2024-04-16 20:57:38 -0300

    Alterações de imagens

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Components from './components/Header/Page.jsx'
+
+function App() {
+
+  return (
+    <>
+      <div className='vh-100 mh-100 bg-primary-santri overflow-x-hidden'>
+        <Components />
+      </div>
+    </>
+  )
+}
+
+export default App
