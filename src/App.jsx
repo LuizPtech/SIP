409bd8250c80e2c1016aee01cfd906f7c81a365b
Luiz Phelipe <55281545+LuizPtech@users.noreply.github.com>
2024-04-22 17:08:06 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import './App.css'
 import Components from './components/Header/Page.jsx'
+import React, { useEffect } from 'react';
 
 function App() {
-
+  useEffect(() => {
+    document.title = "Sip Experience"; // Altere aqui
+  }, []);
   return (
     <>
       <div className='vh-100 mh-100 bg-primary-santri overflow-x-hidden'>
